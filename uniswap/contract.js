718ca50c8312a2953ad61103bed728672a9b3651
Gustavo Nunes <151034795+gustavonunesbispo@users.noreply.github.com>
2024-03-24 13:41:47 +0000

    updating contract calls

--- uniswap/contract.js
+++ uniswap/contract.js
@@ -1,7 +1,14 @@
+const { Web3 } = require('web3');
+const PriceOracleABI = require('./ABI/PriceOracleABI.js'); // Substitua pelo ABI do seu contrato
+const contractAddress = '0xE4CF40158EAF0e5b77CbE94912364eB1c32F1294'; // Substitua pelo endereço do seu contrato
+
 const Uniswap = require('./uniswap.js');
 const Sushi = require('./sushi.js');
 const Quickswap = require('./quickswap.js');
 
+const web3 = new Web3('https://minnet.infura.io/v3/575df068ef554f19aa933b197f194cde');
+
+const priceOracleContract = new web3.eth.Contract(PriceOracleABI, contractAddress);
 
 async function main() {
     const wbtcPriceUniswap = await Uniswap();
@@ -16,13 +23,24 @@ async function main() {
     const initialPrice = await calculateInitialPrice(wbtcPriceUniswap, wbtcPriceSushi, wbtcPriceQuickswap);
     console.log("Preço inicial:", initialPrice);
 
-    // Agora você pode passar o valor de initialPrice para onde for necessário, como para o contrato inteligente
-    // await deployContract(initialPrice);
+    // Atualiza o preço no contrato PriceOracle
+    await updatePrice(initialPrice);
 }
 
 async function calculateInitialPrice(wbtcPriceUniswap, wbtcPriceSushi, wbtcPriceQuickswap) {
     const averagePrice = (wbtcPriceUniswap + wbtcPriceSushi + wbtcPriceQuickswap) / 3;
     return averagePrice;
+}
+
+async function updatePrice(newPrice) {
+    // Obtém a conta do proprietário
+    const accounts = await web3.eth.getAccounts();
+    const owner = accounts[0];
+
+    // Chama a função updatePrice do contrato PriceOracle
+    await priceOracleContract.methods.updatePrice(newPrice).send({ from: owner });
+
+    console.log("Preço atualizado no contrato PriceOracle.");
 }
 
 setInterval(main, 12000);
